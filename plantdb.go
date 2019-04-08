f74a7ce31325f7a2d0f766aed2a1629914a8f07d
jessebl <j.bl@posteo.org>
2019-04-08 11:35:03 -0500

    Add setCommonName()

--- plantdb.go
+++ plantdb.go
@@ -20,13 +20,23 @@ func species(db *sqlx.DB) ([]models.Species, error) {
 	return sS, err
 }
 
+func setCommonName(db *sqlx.DB, speciesId int, cN string) (int64, error) {
+	q := `UPDATE species SET common_name = ? WHERE species_id = ?`
+	res, err := db.Exec(q, cN, speciesId)
+	rA, _ := res.RowsAffected()
+	return rA, err
+}
+
 func main() {
-	db, err := sqlx.Connect("sqlite3", "./plant.db")
+	db, _ := sqlx.Connect("sqlite3", "./plant.db")
 	defer db.Close()
-	if err != nil {
-		fmt.Errorf(err.Error())
-	}
 	sS, _ := speciesFlattened(db)
+	for _, s := range sS {
+		fmt.Println(s)
+	}
+	res, err := setCommonName(db, 2, "yee")
+	fmt.Println("Rows affected:", res, "Errors:", err)
+	sS, _ = speciesFlattened(db)
 	for _, s := range sS {
 		fmt.Println(s)
 	}
